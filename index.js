bbde42cdb46fd63505133975b9266fe979e3ff9b
Yusadolat <yusadolat@gmail.com>
2023-12-01 10:44:42 +0100

    Update HTTP function name

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const functions = require('@google-cloud/functions-framework');
 
 // Register an HTTP function with the Functions Framework that will be executed
 // when you make an HTTP request to the deployed function's endpoint.
-functions.http('devfestIbadan', (req, res) => {
+functions.http('devfest', (req, res) => {
   res.send('Hello DevFest Ibadan!');
 });
